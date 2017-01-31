cb267f2be9b6925ba95ffce8011901bd728a996e
rnorn <rnorn@RNOTOWER>
2017-01-31 23:13:18 +0100

    Add project files.

---
+++ ContratsWebAPI2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ContratsWebAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ContratsWebAPI2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ContratsWebAPI2.Startup))]
+
+namespace ContratsWebAPI2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
